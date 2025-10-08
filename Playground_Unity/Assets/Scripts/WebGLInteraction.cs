ad675e95f0193f9e1375836c0753081fa7e5743a
Nhan Luong <130432957+NhanLuongBGSV@users.noreply.github.com>
2025-10-08 08:37:50 +0000

    Add license header

--- Playground_Unity/Assets/Scripts/WebGLInteraction.cs
+++ Playground_Unity/Assets/Scripts/WebGLInteraction.cs
@@ -1,4 +1,12 @@
-﻿using System.Runtime.InteropServices;
+﻿// Copyright (c) 2025 Eclipse Foundation.
+// 
+// This program and the accompanying materials are made available under the
+// terms of the MIT License which is available at
+// https://opensource.org/licenses/MIT.
+//
+// SPDX-License-Identifier: MIT
+
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class WebGLInteraction : MonoBehaviour
